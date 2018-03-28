66ffbddffa3d8d4a78ddcb85ec233658baacfd3e
Greg Horvath <horvath.105@gmail.com>
2018-03-28 02:59:38 +0000

    Fixed #1 -Fixed burn feed ordering. -Fixed burn load more duplicate issue. -Removed disabling of submit button. Added form validation check which triggers and alert.

--- Actions/GetBurns.php
+++ Actions/GetBurns.php
@@ -2,14 +2,25 @@
 require '../dbHelper.php';
 
 $lastBurnId = (int)$_GET['LastBurnId'];
-$reqLimit = 6;
+$reqLimit = 2;
 
 $conn = dbHelper::createInstance();
 
-$query = "select * from Burn order by Id Limit $reqLimit";
-if (! is_null($lastBurnId)) {
+$minQuery = "select min(Id) from Burn";
+$minId = $conn->query($minQuery)->fetch_array()[0];
+
+$query = "select * from Burn order by Id desc Limit $reqLimit";
+if ($lastBurnId !== 0) {
     //this should be safe as int cast should take care of any sql injection issues.
-    $query = "select * from Burn where Id > $lastBurnId order by Id Limit $reqLimit"; 
+    $query = "select * from Burn where Id < $lastBurnId order by Id desc Limit $reqLimit"; 
+}
+
+//echo $minId;
+//echo $query;
+
+if($lastBurnId != 0 and $lastBurnId <= $minId) {
+    echo "[]";
+    return;
 }
 
 $result = $conn->query($query);
--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-define('DBHOST','localhost');
-define('DBUSER','root');
-define('DBPWD','projectchadrick');
-define('DBNAME','mysql');
-define('DEBUG', true);
-define('ENABLENOTIF', false)
-?>
